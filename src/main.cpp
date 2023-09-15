daa254b8e62553b3a4b6c979acaadc7ae3cf8e94
hayk <haykh.astro@gmail.com>
2023-09-15 13:17:35 -0400

    min version

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,50 @@
-#include <adios2.h>
+#include <Kokkos_Core.hpp>
+#include <stdio.h>
 
+#include <chrono>
 #include <iostream>
-#include <vector>
 
-int main(int argc, char* argv[]) {
-#ifndef ADIOS2_USE_MPI
-  throw std::runtime_error("This example requires MPI");
+#ifdef ADIOS2_ENABLED
+#  include <adios2.h>
+#  include <adios2/cxx11/KokkosView.h>
 #endif
 
-  int rank  = 0;
-  int nproc = 1;
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
-  const int NSTEPS = 5;
-  srand(rank * 32767);
+#ifdef MPI_ENABLED
+#  include <mpi.h>
+#endif
 
-  adios2::ADIOS       adios(MPI_COMM_WORLD);
+namespace math = Kokkos;
 
-  const size_t        Nglobal = 6;
-  std::vector<double> v0(Nglobal);
+auto Initialize(int argc, char* argv[]) -> void;
+auto Finalize() -> void;
 
-  unsigned int        Nelems;
-  std::vector<double> v2;
-
+auto main(int argc, char* argv[]) -> int {
+  Initialize(argc, argv);
   try {
-    adios2::IO io = adios.DeclareIO("Output");
-    io.SetEngine("HDF5");
-    io.SetParameters({
-      {"verbose", "4"}
-    });
-
-    adios2::Variable<double> varV0 = io.DefineVariable<double>("v0", {}, {}, { Nglobal });
-    adios2::Variable<double> varV1
-      = io.DefineVariable<double>("v1", { nproc * Nglobal }, { rank * Nglobal }, { Nglobal });
-    adios2::Variable<double> varV2
-      = io.DefineVariable<double>("v2", {}, {}, { adios2::UnknownDim });
-
-    adios2::Engine writer = io.Open("localArray.h5", adios2::Mode::Write);
-
-    for (int step = 0; step < NSTEPS; step++) {
-      writer.BeginStep();
-      for (size_t i = 0; i < Nglobal; i++) {
-        v0[i] = rank * 1.0 + step * 0.1;
-      }
-      writer.Put<double>(varV0, v0.data());
-      writer.Put<double>(varV1, v0.data());
-
-      Nelems = rand() % 6 + 5;
-      v2.reserve(Nelems);
-      for (size_t i = 0; i < Nelems; i++) {
-        v2[i] = rank * 1.0 + step * 0.1;
-      }
-      varV2.SetSelection(adios2::Box<adios2::Dims>({}, { Nelems }));
-      writer.Put<double>(varV2, v2.data());
-
-      writer.EndStep();
-    }
-
-    writer.Close();
+    int mpi_rank = 0, mpi_size = 1;
+#if defined(MPI_ENABLED)
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+#endif
   } catch (std::exception& e) {
-    std::cout << "ERROR: ADIOS2 exception: " << e.what() << std::endl;
+    std::cerr << "Exception caught: " << e.what() << '\n';
+    Finalize();
+    return 1;
   }
-  MPI_Finalize();
+  Finalize();
   return 0;
+}
+
+auto Initialize(int argc, char* argv[]) -> void {
+  Kokkos::initialize(argc, argv);
+#ifdef MPI_ENABLED
+  MPI_Init(&argc, &argv);
+#endif
+}
+
+auto Finalize() -> void {
+#ifdef MPI_ENABLED
+  MPI_Finalize();
+#endif
+  Kokkos::finalize();
 }
