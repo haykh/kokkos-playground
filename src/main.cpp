8103a61cdf3ba045560b88b391f91d876f5df7c9
haykh <haykh.astro@gmail.com>
2024-06-12 15:36:00 -0500

    test-amd

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <Kokkos_Core.hpp>
 
+#include <iostream>
+
 #ifdef ADIOS2_ENABLED
   #include <adios2.h>
   #include <adios2/cxx11/KokkosView.h>
@@ -15,8 +17,8 @@ auto Initialize(int argc, char* argv[]) -> void;
 auto Finalize() -> void;
 
 auto main(int argc, char* argv[]) -> int {
-  Initialize(argc, argv);
   try {
+    Initialize(argc, argv);
     Playground();
   } catch (std::exception& e) {
     std::cerr << "Exception caught: " << e.what() << '\n';
@@ -28,7 +30,9 @@ auto main(int argc, char* argv[]) -> int {
 }
 
 auto Initialize(int argc, char* argv[]) -> void {
-  Kokkos::initialize(argc, argv);
+  if (!Kokkos::is_initialized()) {
+    Kokkos::initialize(argc, argv);
+  }
 #ifdef MPI_ENABLED
   MPI_Init(&argc, &argv);
 #endif
@@ -38,5 +42,7 @@ auto Finalize() -> void {
 #ifdef MPI_ENABLED
   MPI_Finalize();
 #endif
-  Kokkos::finalize();
+  if (Kokkos::is_initialized()) {
+    Kokkos::finalize();
+  }
 }
