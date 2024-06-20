ee952deb47195346de8fb3953473f339091236a0
haykh <haykh.astro@gmail.com>
2024-06-20 16:19:14 -0400

    sync

--- src/playground.cpp
+++ src/playground.cpp
@@ -1,7 +1,22 @@
 #include "playground.hpp"
 
+#include <Kokkos_Core.hpp>
+
 #include <iostream>
 
 auto Playground() -> void {
-  std::cout << "Hello, world!\n";
+  constexpr int                           N = 100;
+  Kokkos::View<double*, Kokkos::HIPSpace> A { "A", N };
+  Kokkos::parallel_for(
+    Kokkos::RangePolicy<Kokkos::HIP>(0, N),
+    KOKKOS_LAMBDA(int i) {
+      A(i) = 1.0; // / (static_cast<double>(i + 1) * static_cast<double>(i + 1));
+    });
+  // double sum = 0.0;
+  // Kokkos::parallel_reduce(
+  //   N,
+  //   KOKKOS_LAMBDA(int i, double& lsum) { lsum += A(i); },
+  //   sum);
+  // std::cout << "pi = " << Kokkos::sqrt(sum * 6.0) << '\n';
+  // std::cout << "Hello, world!\n";
 }
